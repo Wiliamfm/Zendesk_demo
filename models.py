9569d6f0dc67cfe4b6acb8b261dd6c63f084359f
William Fonseca <santifon15@gmail.com>
2023-02-13 18:51:40 -0500

    update user and users models

--- models.py
+++ models.py
@@ -2,22 +2,26 @@ from pydantic import BaseSettings, BaseModel
 
 
 class Settings(BaseSettings):
-    subdomain: str = "d3v-haggen-it"
-    username: str = "juan@haggen-it.com/token"
-    password: str = "3kBi95xPJJXsFQm7vyEWeXAKzfOsPFyRAT6F1mVO"
+   subdomain: str = "d3v-haggen-it"
+   username: str = "juan@haggen-it.com/token"
+   password: str = "3kBi95xPJJXsFQm7vyEWeXAKzfOsPFyRAT6F1mVO"
+
+
+class User(BaseModel):
+   active: bool | None
+   alias: str | None
+   created_at: str | None
+   details: str | None
+   email: str | None
+   id: int | None
+   last_login_at: str | None
+   name: str | None
+   phone: str | None
+   role: str | None
+   role_type: int | None
+   ticket_restriction: str | None
+   url: str | None
 
 
 class Users(BaseModel):
-    active: bool
-    alias: str
-    chat_only: bool
-    created_at: str
-    custom_role_id: int
-    default_group_id: int
-    details: str
-    email: str
-    external_id: str
-    iana_time_zone: str
-    id: int
-    last_login_at: str
-    locale: str
+   users: list[User]
