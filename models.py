da35d1ae762b7243a2eb838ede6b6d27786d4f25
William Fonseca <santifon15@gmail.com>
2023-02-13 18:32:09 -0500

    add users model

--- models.py
+++ models.py
@@ -1,7 +1,23 @@
-from pydantic import BaseSettings
+from pydantic import BaseSettings, BaseModel
 
 
 class Settings(BaseSettings):
     subdomain: str = "d3v-haggen-it"
     username: str = "juan@haggen-it.com/token"
     password: str = "3kBi95xPJJXsFQm7vyEWeXAKzfOsPFyRAT6F1mVO"
+
+
+class Users(BaseModel):
+    active: bool
+    alias: str
+    chat_only: bool
+    created_at: str
+    custom_role_id: int
+    default_group_id: int
+    details: str
+    email: str
+    external_id: str
+    iana_time_zone: str
+    id: int
+    last_login_at: str
+    locale: str
