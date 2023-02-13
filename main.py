96fae449da880c65305243ae90de3892aa829e45
William Fonseca <santifon15@gmail.com>
2023-02-13 18:52:02 -0500

    add get users by role function

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from typing import Union
 
 from fastapi import FastAPI
 import models
+import utils
 
 settings = models.Settings()
 app = FastAPI()
@@ -9,14 +10,17 @@ app = FastAPI()
 
 @app.get("/users")
 def list_users():
-    return {"Hello": "World"}
+   users = utils.get_users_by_role(
+       f"{settings.subdomain}.zendesk.com", settings.username, settings.password, "end-user")
+   print(type(users))
+   return users
 
 
 @app.get("/info")
 async def info():
-    return settings
+   return settings
 
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+   return {"item_id": item_id, "q": q}
--- utils.py
+++ utils.py
@@ -1,5 +1,9 @@
-import requests
+import requests as req
+import models
 
 
-def get_users():
-    pass
+def get_users_by_role(domain: str, user: str, pwd: str, role: str):
+   r = req.get(
+       f"https://{domain}/api/v2/users?role={role}", auth=(user, pwd))
+   users: models.Users = models.Users(**r.json())
+   return users
