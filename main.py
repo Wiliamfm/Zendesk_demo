4bd00ffd6849bb99dfccf6813656a83041909535
William Fonseca <santifon15@gmail.com>
2023-02-13 19:10:21 -0500

    refactor list_users endpoint

--- main.py
+++ main.py
@@ -12,15 +12,9 @@ app = FastAPI()
 def list_users():
    users = utils.get_users_by_role(
        f"{settings.subdomain}.zendesk.com", settings.username, settings.password, "end-user")
-   print(type(users))
    return users
 
 
 @app.get("/info")
 async def info():
    return settings
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-   return {"item_id": item_id, "q": q}
