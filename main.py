88323b792416c9acb4c0288fb44b8156d3ee4c88
William Fonseca <santifon15@gmail.com>
2023-02-13 19:34:40 -0500

    add next, prev links to list user endpoint

--- main.py
+++ main.py
@@ -9,9 +9,14 @@ app = FastAPI()
 
 
 @app.get("/users")
-def list_users(role: str = "end-user", pagination: int | None = None):
+def list_users(role: str = "end-user", pagination: int | None = None, next: str | None = None, prev: str | None = None):
+   url: str | None = None
+   if prev:
+      url = prev
+   if next:
+      url = next
    users = utils.get_users_by_role(
-       f"{settings.subdomain}.zendesk.com", settings.username, settings.password, role, pagination)
+       url, f"{settings.subdomain}.zendesk.com", settings.username, settings.password, role, pagination)
    return users
 
 
--- utils.py
+++ utils.py
@@ -2,11 +2,12 @@ import requests as req
 import models
 
 
-def get_users_by_role(domain: str, user: str, pwd: str, role: str, pagination: int | None):
-   if (not pagination):
-      url = f"https://{domain}/api/v2/users?role={role}"
-   else:
-      url = f"https://{domain}/api/v2/users?role={role}&page[size]={pagination}"
+def get_users_by_role(url: str | None, domain: str, user: str, pwd: str, role: str, pagination: int | None):
+   if (not url):
+      if (not pagination):
+         url = f"https://{domain}/api/v2/users?role={role}"
+      else:
+         url = f"https://{domain}/api/v2/users?role={role}&page[size]={pagination}"
    r = req.get(url, auth=(user, pwd))
    print(r.json())
    users: models.Users_Response = models.Users_Response(**r.json())
